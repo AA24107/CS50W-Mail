e70865722274138582c27b71abfa2226156b89d0
Afeefa Ansari <ansariafeefa7860@gmail.com>
2026-02-06 21:24:31 +0530

    Compose_email fuctionality

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(emails => {
+      console.log(emails);
+  });
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
@@ -12,6 +16,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function compose_email() {
 
+  document.querySelector('#compose-form').onsubmit =() => {
+    const recipients = document.querySelector("#compose-recipients").value;
+    const subject = document.querySelector("#compose-subject").value;
+    const body = document.querySelector("#compose-body").value;
+
+    fetch("/emails", {
+      method: "POST",
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body,
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+    });
+    load_mailbox('sent');
+    return false;
+  };
+
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -23,7 +48,11 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(result =>{
+    console.log(result)
+  })
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
